07d95645637da31c776a628c6028572d65161ca6
Codemonkey51 <62217716+Codemonkey51@users.noreply.github.com>
2021-01-08 08:16:55 -0800

    Import backend api in main.py
    In the reformat import api was never re-added, so the api wasn't working. Fixed with this commit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ if DOT_ENV == False:
 	load_dotenv()
 
 # Don't move this up, env must be loaded first
+from repldex.backend import api
+
 from repldex.discordbot import bot as discordbot
 from repldex.backend import website
 
