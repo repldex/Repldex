8a315ccc5927e12383dcf5136302e6476b998ff6
Leon ge <leon332157@gmail.com>
2021-01-10 06:39:25 +0000

    (Leon's Commit) remove PATH to avoid confusion

--- main.sh
+++ main.sh
@@ -2,7 +2,6 @@ unset PYTHONPATH
 unset POETRY_VIRTUALENVS_PATH
 unset VIRTUAL_ENV #/opt/virtualenvs/python3
 export PYTHONPATH='/usr/local/lib/python3.8/site-packages'
-export PATH='/home/runner/.apt/usr/bin:/home/runner/.local/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 /usr/local/bin/python3 -m poetry
 if [ $? -ne 0 ]; then
   echo "Installing poetry"
