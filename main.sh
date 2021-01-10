e4c5b12ae16983baf7e315c922e7d46513616d43
mat <replit@matdoes.dev>
2021-01-10 06:26:21 +0000

    added a bash script to allow for faster loading with venv

--- main.sh
+++ main.sh
@@ -3,8 +3,13 @@ unset POETRY_VIRTUALENVS_PATH
 unset VIRTUAL_ENV #/opt/virtualenvs/python3
 export PYTHONPATH='/usr/local/lib/python3.8/site-packages'
 export PATH='/home/runner/.apt/usr/bin:/home/runner/.local/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-#/usr/local/bin/python3 -m pip install poetry pyparsing --user
-#/opt/virtualenvs/python3/bin/python3 -m poetry install --no-root --no-dev -v
+/usr/local/bin/python3 -m poetry
+if [ $? -ne 0 ]; then
+  echo "Installing poetry"
+  /usr/local/bin/python3 -m pip install poetry --user
+fi
+clear
+echo "Installing dependencies"
 #(set -o posix ; set)
 /usr/local/bin/python3 -m poetry install --no-root --no-dev -v
 /home/runner/ReplDex/.venv/bin/python main.py
