ba08a769d11fac0e83b3e6e21488acb614456e1c
leon332157 <leon332157@gmail.com>
2021-01-10 02:02:42 -0500

    Fix wrong command

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ unset PYTHONPATH
 unset POETRY_VIRTUALENVS_PATH
 unset VIRTUAL_ENV #/opt/virtualenvs/python3
 export PYTHONPATH='/usr/local/lib/python3.8/site-packages'
-/usr/local/bin/python3 -m pip check poetry
+/usr/local/bin/python3 -m pip show poetry
 if [ $? -ne 0 ]; then
   echo "Installing poetry"
   /usr/local/bin/python3 -m pip install poetry --user
