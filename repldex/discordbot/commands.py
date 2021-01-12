ccb3bf46d4d5293b3aaf384bfaff2d6e93a005ba
leon332157 <leon332157@gmail.com>
2021-01-12 12:52:33 -0500

    make ping admin only

--- repldex/discordbot/commands.py
+++ repldex/discordbot/commands.py
@@ -10,6 +10,7 @@ blacklist_wait = 5
 
 print('commands')
 
+
 @betterbot.command(name='help', allowed=True)
 async def help(message):
 	if message.message.author.id in BLACKLIST_IDS:
@@ -303,6 +304,9 @@ async def random_entry(message):
 
 @betterbot.command(name='ping', aliases=['pong', 'pung'])
 async def ping(message):
+	if message.author.id not in ADMIN_IDS:
+		return
+
 	ping = client.latency * 1000
 	await message.send(embed=discord.Embed(title='Ping!', description=f'Bot ping is: `{int(ping)}ms`'))
 
