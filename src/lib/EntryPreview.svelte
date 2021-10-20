66021f1331a0f8c12a202419ed61db1dffbc90a4
mat <27899617+mat-1@users.noreply.github.com>
2021-10-20 15:38:16 +0000

    Add a border around the entry preview

--- src/lib/EntryPreview.svelte
+++ src/lib/EntryPreview.svelte
@@ -3,7 +3,19 @@
 	export let entry
 </script>
 
-<div>
-	<h2>{entry.title}</h2>
+<div class="entry-preview-container">
+	<h2 class="entry-preview-title">{entry.title}</h2>
 	<p>{entry.description}</p>
 </div>
+
+<style>
+	.entry-preview-container {
+		border: 1px solid var(--alternate-background-color);
+		border-radius: 1em;
+		padding: 1em
+	}
+
+	.entry-preview-title {
+		margin-top: 0;
+	}
+</style>
