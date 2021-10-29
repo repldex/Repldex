599640b758ca92f0255a7affd6d9a0a50883de89
selectthegang <66577268+selectthegang@users.noreply.github.com>
2021-10-29 00:17:21 +0000

    removed image

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -2,11 +2,9 @@
 	export let title: string = 'Repldex'
 	export let description: string =
 		'Repldex is the unofficial community-editable encyclopedia of user created entries for the Replit Discord Server'
-	export let image: string = '/icon.png'
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	<meta name="og:image" content={image} />
 </svelte:head>
