aaf638e3b72c9d275c091c8bc75e96e0b210e0ca
selectthegang <66577268+selectthegang@users.noreply.github.com>
2021-11-09 00:03:29 +0000

    Fixed requested issues

--- src/lib/discord/bot.ts
+++ src/lib/discord/bot.ts
@@ -1,45 +1,45 @@
-import { Command, ApplicationCommandOptionType } from "./api/commands";
+import { Command, ApplicationCommandOptionType } from './api/commands'
 
 new Command({
-  name: "entry",
-  description: "View a Repldex entry",
+	name: 'entry',
+	description: 'View a Repldex entry',
 })
-  .addOption({
-    name: "name",
-    description: "The name of the entry",
-    type: ApplicationCommandOptionType.String,
-    required: true,
-  })
-  .handle((i) => {
-    console.log(i);
-  });
+	.addOption({
+		name: 'name',
+		description: 'The name of the entry',
+		type: ApplicationCommandOptionType.String,
+		required: true,
+	})
+	.handle(i => {
+		console.log(i)
+	})
 
 new Command({
-  name: "selfentry",
-  description: "View a Repldex entry via owner username",
+	name: 'selfentry',
+	description: 'View a Repldex entry via owner username',
 })
-  .addOption({
-    name: "User",
-    description: "The owner of the entry",
-    type: ApplicationCommandOptionType.User,
-    required: true,
-  })
-  .handle((i) => {
-    console.log(i);
-  });
+	.addOption({
+		name: 'User',
+		description: 'The owner of the entry',
+		type: ApplicationCommandOptionType.User,
+		required: true,
+	})
+	.handle(i => {
+		console.log(i)
+	})
 
 new Command({
-  name: "random",
-  description: "View a random Repldex entry",
-}).handle((i) => {
-  console.log(i);
-});
+	name: 'random',
+	description: 'View a random Repldex entry',
+}).handle(i => {
+	console.log(i)
+})
 
 new Command({
-  name: "featured",
-  description: "View the featured Repldex entry",
-}).handle((i) => {
-  console.log(i);
-});
+	name: 'featured',
+	description: 'View the featured Repldex entry',
+}).handle(i => {
+	console.log(i)
+})
 
-console.log("bot");
+console.log('bot')
