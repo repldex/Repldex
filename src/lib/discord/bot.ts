bcf44026e2fb42c8312f531978f45b593cfa9270
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2021-11-20 13:22:00 -0800

    removes newentry, adds source command

--- src/lib/discord/bot.ts
+++ src/lib/discord/bot.ts
@@ -29,23 +29,6 @@ new Command({
 	})
 
 new Command({
-	name: 'newentry',
-	description: 'Get link to make a new entry',
-})
-	.addOption({
-		name: 'name',
-		description: 'The name of the new entry',
-		type: ApplicationCommandOptionType.String,
-		required: true,
-	})
-	.handle(i => {
-		console.log(i)
-		return {
-			content: "Ok"
-		}
-	})
-
-new Command({
 	name: 'search',
 	description: 'Search Repldex entries',
 })
@@ -69,7 +52,48 @@ new Command({
 	.handle(i => {
 		console.log(i)
 		return {
-			content: "Ok"
+			embeds: [
+				{
+					title: "Source",
+					description: "My source code is on [Github](https://github.com/mat-1/ReplDex)",
+					color: "#650ac9",
+					fields: [
+						{
+							name: "Mat1",
+							value: "Project Head",
+							inline: true
+						},
+						{
+							name: "Coderman51",
+							value: "Core Contributor",
+							inline: true
+						},
+						{
+							name: "Prussia",
+							value: "Discord Bot Developer",
+							inline: true
+						},
+						{
+							name: "Nayoar",
+							value: "Site Administrator",
+							inline: true
+						},
+						{
+							name: "Kognise",
+							value: "Owns the domain",
+							inline: true
+						},
+						{
+							name: "Selectthemat",
+							value: "Major contributor",
+							inline: true
+						}
+					],
+					footer: {
+						text: "Also big thanks to all the editors and other contributors"
+					}
+				}
+			]
 		}
 	})
 
