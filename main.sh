04540118207ea4fa52316ddeb853a2a7435e7921
leon332157 <leon332157@gmail.com>
2021-11-25 13:38:42 -0500

    add git pull in script

--- main.sh
+++ main.sh
@@ -4,6 +4,11 @@ export PYTHONPATH='/usr/local/lib/python3.8/site-packages'
 export POETRY_VIRTUALENVS_PATH='/home/runner/.venv'
 export POETRY_VIRTUALENVS_CREATE='true'
 /usr/local/bin/python3 -m pip show poetry
+pull_git() {
+    echo "git pulling"
+    git reset --hard
+    git pull origin master
+}
 if [ $? -ne 0 ]; then
     echo "Installing poetry"
     /usr/local/bin/python3 -m pip install poetry --user
@@ -15,6 +20,7 @@ if [ ! -d "/home/runner/.venv" ];then
     
 fi
 while true;do
+    pull_git
     /usr/local/bin/python3 -m poetry run python -OO -X pycache_prefix='__pycache__' -X tracemalloc main.py
 done
 #(set -o posix ; set)
