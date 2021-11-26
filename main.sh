caffe3280dd4a49c6498d6005e071cc361c113b7
leon332157 <leon332157@gmail.com>
2021-11-25 21:10:47 -0500

    fix looping

--- main.sh
+++ main.sh
@@ -4,11 +4,6 @@ export PYTHONPATH='/usr/local/lib/python3.8/site-packages'
 export POETRY_VIRTUALENVS_PATH='/home/runner/.venv'
 export POETRY_VIRTUALENVS_CREATE='true'
 /usr/local/bin/python3 -m pip show poetry
-pull_git() {
-    echo "git pulling"
-    git reset --hard
-    git pull origin main
-}
 if [ $? -ne 0 ]; then
     echo "Installing poetry"
     /usr/local/bin/python3 -m pip install poetry --user
@@ -20,7 +15,9 @@ if [ ! -d "/home/runner/.venv" ];then
     
 fi
 while true;do
-    pull_git
+    echo "git pulling"
+    git reset --hard
+    git pull origin main
     /usr/local/bin/python3 -m poetry run python -OO -X pycache_prefix='__pycache__' -X tracemalloc main.py
 done
 #(set -o posix ; set)
