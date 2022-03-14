d65fcc286ee49532ff7f078ed32c9c00ef2447fb
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2022-03-14 16:03:35 -0700

    comply with eslint

--- src/routes/api/random.json.ts
+++ src/routes/api/random.json.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from '@sveltejs/kit'
 import type { JSONValue } from '@sveltejs/kit/types/helper'
 
 // random entry
-export const get: RequestHandler = async req => {
+export const get: RequestHandler = async _req => {
 	const entries = await fetchEntries({
 		limit: 1,
 		skip: Math.floor(Math.random() * (await countEntries())),
