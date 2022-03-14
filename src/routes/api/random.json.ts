57cce0e06d86a19186a41e30d2469fbd3e165fce
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2022-03-14 14:58:43 -0700

    add search and random for api

---
+++ src/routes/api/random.json.ts
@@ -0,0 +1,15 @@
+import { fetchEntries } from '../../lib/database/entries'
+import type { RequestHandler } from '@sveltejs/kit'
+import type { JSONValue } from '@sveltejs/kit/types/helper'
+
+// random entry
+export const get: RequestHandler = async req => {
+	const entries = await fetchEntries({
+		limit: 0,
+		skip: 0,
+	})
+	const entry = entries[Math.floor(Math.random() * entries.length)]
+	return {
+		body: entry as unknown as JSONValue,
+	}
+}
