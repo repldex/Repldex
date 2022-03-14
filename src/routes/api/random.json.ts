8954099fc2dd57980db3242e9d9d9f425fe30be3
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2022-03-14 16:01:30 -0700

    remove search, improve random

--- src/routes/api/random.json.ts
+++ src/routes/api/random.json.ts
@@ -1,14 +1,14 @@
-import { fetchEntries } from '../../lib/database/entries'
+import { fetchEntries, countEntries } from '../../lib/database/entries'
 import type { RequestHandler } from '@sveltejs/kit'
 import type { JSONValue } from '@sveltejs/kit/types/helper'
 
 // random entry
 export const get: RequestHandler = async req => {
 	const entries = await fetchEntries({
-		limit: 0,
-		skip: 0,
+		limit: 1,
+		skip: Math.floor(Math.random() * (await countEntries())),
 	})
-	const entry = entries[Math.floor(Math.random() * entries.length)]
+	const entry = entries[0]
 	return {
 		body: entry as unknown as JSONValue,
 	}
